4d3e9c2b7a6fa710957a9cc562cbd189b88b78aa
Zhu Zhan Yan <program.nom@gmail.com>
2018-12-24 09:32:01 +0800

    test

--- stylenet/stylenet.py
+++ stylenet/stylenet.py
@@ -17,7 +17,7 @@ from tensorflow.contrib.opt import ScipyOptimizerInterface
 from shutil import rmtree
 
 # Model Settings
-IMAGE_DIM = (800, 800)
+IMAGE_DIM = (512, 512)
 INPUT_SHAPE = (None, IMAGE_DIM[0], IMAGE_DIM[1], 3)
  
 CONTENT_WEIGHT = 1e-4
